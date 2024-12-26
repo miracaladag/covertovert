9b0c28f93d852bdf7d49fa7f3b169c43ceea257b
SelTarARI <seltarari@gmail.com>
2024-12-26 03:29:35 +0300

    implemantation

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -1,25 +1,91 @@
 from CovertChannelBase import CovertChannelBase
+import struct
+from scapy.all import IP, UDP, Raw, sniff
 
 class MyCovertChannel(CovertChannelBase):
-    """
-    - You are not allowed to change the file name and class name.
-    - You can edit the class in any way you want (e.g. adding helper functions); however, there must be a "send" and a "receive" function, the covert channel will be triggered by calling these functions.
-    """
     def __init__(self):
-        """
-        - You can edit __init__.
-        """
-        pass
+        super().__init__()
+
     def send(self, log_file_name, parameter1, parameter2):
         """
-        - In this function, you expected to create a random message (using function/s in CovertChannelBase), and send it to the receiver container. Entire sending operations should be handled in this function.
-        - After the implementation, please rewrite this comment part to explain your code basically.
+        This function generates a random binary message and sends it using the Mode field in NTP packets.
         """
+        # Generate a random binary message with logging
         binary_message = self.generate_random_binary_message_with_logging(log_file_name)
-        
+        binary_message += '.'  # Append '.' as a breaker to signal the end of communication
+
+        # Use parameter1 as server address and parameter2 as port if specified
+        ntp_server = parameter1
+        port = parameter2
+
+        for bit in binary_message:
+            mode = 7 if bit == '.' else int(bit)  # Use mode 7 for the breaker, otherwise the bit value
+            packet = self.create_ntp_packet(mode)
+            self.send_ntp_packet(ntp_server, port, packet)
+
     def receive(self, parameter1, parameter2, parameter3, log_file_name):
         """
-        - In this function, you are expected to receive and decode the transferred message. Because there are many types of covert channels, the receiver implementation depends on the chosen covert channel type, and you may not need to use the functions in CovertChannelBase.
-        - After the implementation, please rewrite this comment part to explain your code basically.
+        This function receives NTP packets and decodes the message from the Mode field.
         """
-        self.log_message("", log_file_name)
+        received_message = []
+        self.stop_sniffing = False  # Flag to stop sniffing
+
+        def packet_callback(packet):
+            if packet.haslayer(UDP) and packet[UDP].sport == parameter2:
+                mode = self.extract_mode_from_packet(bytes(packet[UDP].payload))
+                received_message.append(str(mode))  # Add mode to received message
+                if mode == 7:  # Stop when breaker mode is detected
+                    self.stop_sniffing = True  # Set flag to stop sniffing
+
+        # Sniff filter for UDP traffic on the specified port
+        sniff_filter = f"udp port {parameter2}"
+
+        # Sniff packets and use stop_filter to monitor stopping condition
+        sniff(
+            filter=sniff_filter,
+            prn=packet_callback,
+            stop_filter=lambda _: self.stop_sniffing,  # Stop based on the flag
+            #timeout=10  # Optional timeout for safety
+        )
+
+        # Decode the received message, removing the breaker '7' if present
+        binary_message = ''.join(received_message).replace('7', '')  # Remove breaker '7'
+        decoded_message = ''.join(self.convert_eight_bits_to_character(binary_message[i:i+8]) 
+                                  for i in range(0, len(binary_message), 8))
+
+        # Log the received message
+        self.log_received_message(log_file_name, decoded_message)
+
+    def create_ntp_packet(self, mode):
+        """
+        Create an NTP packet with the specified mode.
+        """
+        # NTP packet format: https://tools.ietf.org/html/rfc5905
+        # Mode is in the last 3 bits of the first byte
+        first_byte = (0 << 6) | (3 << 3) | mode  # LI = 0, VN = 3, Mode = mode
+        packet = struct.pack('!B', first_byte) + b'\x00' * 47
+        return packet
+
+    def send_ntp_packet(self, ntp_server, port, packet):
+        """
+        Send an NTP packet to the specified server and port using the provided send function.
+        """
+        ip = IP(dst=ntp_server)
+        udp = UDP(sport=123, dport=port)
+        raw = Raw(load=packet)
+        CovertChannelBase.send(self, ip/udp/raw)
+
+    def extract_mode_from_packet(self, packet):
+        """
+        Extract the mode field from an NTP packet.
+        """
+        first_byte = struct.unpack('!B', packet[:1])[0]
+        mode = first_byte & 0x07  # Last 3 bits
+        return mode
+
+    def log_received_message(self, log_file_name, message):
+        """
+        Log the received message to a file.
+        """
+        with open(log_file_name, 'w') as log_file:
+            log_file.write(message)
--- code/README.md
+++ code/README.md
@@ -1,3 +1,1 @@
-# Mode Field in NTP
-
-Explain your study in detail as you share your work with the community in a public repository. Anyone should understand your project when read it without having a previous information about the homework.
+# Protocol Field Manipulation using Mode field in NTP
